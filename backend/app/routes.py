3e58d978d8b86591904cb7a090ad633f48ad1093
Centy26 <centrineatieno20@gmail.com>
2024-11-09 08:01:27 +0300

    route to login user

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -16,3 +16,31 @@ def register():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
+
+
+    #check if user already exists
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message:' 'User already exists'}), 400
+
+    #Hash the password and create user
+    password_hash = generate_password_hash(password)    
+    new_user = User (username = username, password_hash = password_hash)
+
+    db.sesssion.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User registered successfully'}), 201
+
+#Route to login user
+@bp.route('/login', methods = ['POST'])    
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if user and check_password_hash(user.password_hash, password):
+        session['user_id'] = user.user_id
+        return jsonify({'message': 'Login successful'}), 200
+    return jsonify({'message': 'Invalid username or password'}), 401
+        
