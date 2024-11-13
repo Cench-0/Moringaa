348b2eca0784d3deebbfd86193785a37c569e856
Centy26 <centrineatieno20@gmail.com>
2024-11-13 22:52:15 +0300

    added app.js file

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header'; 
+import Sidebar from './components/Sidebar'; 
+import PairingDashboard from './components/PairingDashboard';
+import History from './components/History'; 
+import Login from './components/Login'; 
+import Register from './components/Register'; 
+import UserProfile from './components/UserProfile'; 
+import Pairing from './components/Pairing'; 
+
+
+function App() {
+    return (
+      <div className="app">
+        <Header />
+        <div className="main-content">
+          <Sidebar />
+          <div className="content">
+            <Switch>
+              <Route path="/" exact component={PairingDashboard} />
+              <Route path="/history" component={History} />
+              <Route path="/profile" component={UserProfile} />
+              <Route path="/pairing" component={Pairing} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
